8e15c02818764a12124fc2f7717aad3c6c25ceb6
Varun-18 <varunshah735@gmail.com>
2023-06-15 12:15:58 +0530

    loading improved

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,9 +4,34 @@ import { wrapper } from "@src/store";
 import { ThemeProvider } from "@material-tailwind/react";
 import Header from "@src/components/Header";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Loading from "@src/components/Loading";
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("finished");
+      setLoading(false);
+    };
+    router.events.on("routeChangeStart", start);
+    router.events.on("routeChangeError", end);
+    router.events.on("routeChangeComplete", end);
+    return () => {
+      router.events.off("routeChangeStart", start);
+      router.events.off("routeChangeComplete", end);
+      router.events.off("routeChangeError", end);
+    };
+  }, []);
+
   return (
     <SessionProvider>
       <ThemeProvider>
@@ -23,7 +48,7 @@ function MyApp({ Component, pageProps }) {
           theme="light"
         />
         <Header />
-        <Component {...pageProps} />
+        {loading ? <Loading /> : <Component {...pageProps} />}
       </ThemeProvider>
     </SessionProvider>
   );
